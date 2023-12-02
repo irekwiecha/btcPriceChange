beb5fae13d5aa9dc309cf2b522f9d92f7ef2158d
irekwiecha <irek88@gmail.com>
2023-12-02 09:35:39 +0100

    feat: send sms when the price changes by more than a certain percentage

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import json
+from datetime import datetime, timedelta
+
 import requests
 from twilio.rest import Client
 
@@ -11,24 +13,41 @@ with open("priv.json") as f:
     twilio_nr = priv["twilio_nr"]
     my_nr = priv["my_nr"]
 
-
+PRICE_DIFF = 5
 btc_endpoint = "https://api.coingecko.com/api/v3/coins/bitcoin"
+last24h = datetime.strftime(datetime.now() - timedelta(1), "%Y%m%dT%H%M")
 
 news_parameters = {
     "function": "NEWS_SENTIMENT",
     "tickers": "CRYPTO:BTC",
     "sortBy": "popularity",
     "apikey": APIkey,
-    "time_from": "20231201T0000"
+    "time_from": f"{last24h}",
 }
 news_endpoint = "https://www.alphavantage.co/query?"
 
 resp_btc = requests.get(btc_endpoint)
 resp_btc.raise_for_status()
-btc_data = resp_btc.json()["market_data"]["price_change_percentage_24h"]
-print(btc_data)
+btc_change = resp_btc.json()["market_data"]["price_change_percentage_24h"]
 
 resp_news = requests.get(news_endpoint, params=news_parameters)
 resp_news.raise_for_status()
-news = resp_news.json()
-print(news)
+news_data = resp_news.json()["feed"][:3]
+
+if btc_change >= abs(PRICE_DIFF):
+    for _ in range(3):
+        sentiment_score = f"{news_data[_]['overall_sentiment_label']} | Price change: {btc_change:.2f}%"
+        headline = news_data[_]["title"]
+        summary = news_data[_]["summary"]
+        source = news_data[_]
+        sms_text = (
+            f"Sentiment: {sentiment_score}\nHeadline: {headline}\nSummary: {summary}"
+        )
+
+        client = Client(account_sid, auth_token)
+
+        message = client.messages.create(
+            from_=twilio_nr,
+            body=sms_text,
+            to=my_nr,
+        )
